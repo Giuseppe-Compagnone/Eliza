c57c133e4193e64a713d811339e797efa7d4a941
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2025-10-07 15:32:10 +0200

    author

--- src/cli/__main__.py
+++ src/cli/__main__.py
@@ -4,6 +4,8 @@ Entrypoint for ELIZA CLI.
 Allows running the chatbot using `python -m src.cli`.
 """
 
+__author__ = "Giuseppe Compagnone"
+
 from .terminal import main  # importa la funzione main dal terminale
 
 if __name__ == "__main__":
